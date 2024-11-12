9bd47d36753276857f6a19ab31330c5d01fd6c88
Niels van Dijk <niels@nielsvandijk.eu>
2024-11-12 10:55:56 +0100

    User model toevoegen

--- src/models.py
+++ src/models.py
@@ -1,6 +1,14 @@
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String
 
 
 class Base(DeclarativeBase):
     pass
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
