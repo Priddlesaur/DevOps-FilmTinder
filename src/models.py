ad359c095b78f6d9f557aad078f30a97e5fd25e1
GerbenWNOVI <g.wellink@novi-education.nl>
2024-11-12 13:59:52 +0100

    class movie aangemaakt.

--- src/models.py
+++ src/models.py
@@ -1,6 +1,15 @@
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     pass
 
+class Movie(Base):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+    runtime = Column(Integer)
+    imdb_id = Column(Integer)
