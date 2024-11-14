be0244a995c074fb53df19a63819aafbd8373017
Niels van Dijk <niels@nielsvandijk.eu>
2024-11-14 09:29:08 +0100

    Voeg sqlalchemy en psycopg2-binary toe

--- src/database.py
+++ src/database.py
@@ -1,6 +1,11 @@
 import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+# Load environment variables
+load_dotenv()
 
 # Get database url with dotenv
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test_db.db")
