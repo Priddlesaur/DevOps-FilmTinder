ca46107d7a3e9bbffb2dec9d6e586ef06b170a5d
Niels van Dijk <niels@nielsvandijk.eu>
2024-11-14 14:32:28 +0100

    Add a few genre routes and dto

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from database import get_db
+from routers import genres
 
 # Load environment variables
 load_dotenv()
 
 # Create FastAPI instance
 app = FastAPI()
+app.include_router(genres.router)
 
 @app.get("/")
 async def root(db: Session = Depends(get_db)):
