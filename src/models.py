3386f9ad0380e7d7870e182bb59545d35051d9a7
Foraat <foraat@live.nl>
2024-11-14 12:54:44 +0100

    Ratings model

--- src/models.py
+++ src/models.py
@@ -1,6 +1,14 @@
+from sqlalchemy import nullsfirst, Integer, Date, Column, ForeignKey
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     pass
 
+class Rating(Base):
+    __tablename__ = 'ratings'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    movie_id = Column(Integer, ForeignKey('movies.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    rating = Column(Integer, nullable=False)
+    date = Column(Date, nullable=False)
