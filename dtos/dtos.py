81fea563572039d082eead7d8aee7f6ee40e9b3a
Sophianerim <sophianerim@gmail.com>
2024-11-25 13:25:22 +0100

    Users endpoints toegevoegd

--- dtos/dtos.py
+++ dtos/dtos.py
@@ -1,8 +1,16 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
+from typing import Optional
 
 
 class GenreDto(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-
     id: int
     name: str
+
+class UserDto(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: Optional[int] = Field(None, read_only=True)
+    username: str
+    first_name: str
+    last_name: str
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from database import get_db
-from routers import genres
+from routers import genres, users
 
 # Load environment variables
 load_dotenv()
@@ -10,7 +10,7 @@ load_dotenv()
 # Create FastAPI instance
 app = FastAPI()
 app.include_router(genres.router)
-
+app.include_router(users.router)
 @app.get("/")
 async def root(db: Session = Depends(get_db)):
     return {"message": "Hello World"}
