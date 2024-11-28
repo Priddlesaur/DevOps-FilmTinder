fa3f265c00dcc45c97bfbfa2d173c6d7207e4daa
Niels van Dijk <niels@nielsvandijk.eu>
2024-11-28 11:00:05 +0100

    Add httpx and move auto migrations to database file

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 import os
 
+from alembic.config import Config
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from alembic import command
 
 # Load environment variables
 load_dotenv()
@@ -13,6 +15,11 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test_db.db")
 # Define engine and session
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Run migrations
+alembic_cfg = Config("alembic.ini")
+alembic_cfg.set_main_option("sqlalchemy.url", DATABASE_URL)
+command.upgrade(alembic_cfg, "head")
 
 # Function to get database session
 def get_db():
--- main.py
+++ main.py
@@ -9,11 +9,6 @@ from alembic import command
 # Load environment variables
 load_dotenv()
 
-# Run alembic upgrade head to apply migrations
-alembic_cfg = Config("alembic.ini")
-alembic_cfg.set_main_option("sqlalchemy.url", DATABASE_URL)
-command.upgrade(alembic_cfg, "head")
-
 # Create FastAPI instance
 app = FastAPI()
 app.include_router(genres.router)
