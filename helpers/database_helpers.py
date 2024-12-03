954af04c04e0164a090b5a17f29aa3b5a59d6424
Sophianerim <sophianerim@gmail.com>
2024-12-03 06:11:03 +0100

    Helper functies gemaakt, alle endpoints afgestemd op de helper functies, alles gelijk gezet.

--- helpers/database_helpers.py
+++ helpers/database_helpers.py
@@ -1,10 +1,82 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
+def get_all_entities(entity_class, db: Session):
+    """
+    Helper function that retrieves all entities from the provided data.
+    """
+    return db.query(entity_class).all()
+
+def get_entity(entity_class, entity_id: int, db: Session):
+    """
+    Helper function that retrieves an entity from the provided data by its ID.
+    """
+    entity = db.query(entity_class).get(entity_id)
+    if not entity:
+        raise HTTPException(status_code=404, detail=f"Entity not found")
+    return entity
+
+def create_or_rollback(entity_class, entity_data: dict, db: Session):
+    """
+    Helper function to create an entity in the database using the provided data.
+    Some classes include foreign keys, which may raise exceptions in some cases.
+    """
+    new_entity = entity_class(**entity_data)
+    try:
+        db.add(new_entity)
+        db.commit()
+        db.refresh(new_entity)
+    except IntegrityError as err:
+        db.rollback()
+        if "foreign key constraint" in str(err.orig).lower():
+            raise HTTPException(status_code=400, detail="Invalid foreign key value")
+        raise HTTPException(status_code=400, detail="Database constraint error")
+    except KeyError as err:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Invalid field: {str(err)}")
+    except Exception as err:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(err))
+    return new_entity
+
+def update_or_rollback(instance: object, updates: dict, db: Session):
+    """
+    Helper function that updates an entity from the provided data by its ID.
+    Some classes include foreign keys, which may raise exceptions in some cases.
+    """
+    if not updates:
+        raise HTTPException(status_code=400, detail="No valid fields for update")
+
+    for key,value in updates.items():
+        setattr(instance, key, value)
+
+    try:
+        db.commit()
+        db.refresh(instance)
+    except IntegrityError as err:
+        db.rollback()
+        if "foreign key constraint" in str(err.orig).lower():
+            raise HTTPException(status_code=400, detail="Invalid foreign key value")
+        raise HTTPException(status_code=400, detail="Database constraint error")
+    except KeyError as err:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Invalid field: {str(err)}")
+    except Exception as err:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=str(err))
+    return instance
 
 def delete_or_rollback(instance: object, db: Session):
+    """
+    Helper function that deletes an entity by its ID.
+    """
     try:
         db.delete(instance)
         db.commit()
-    except Exception as err:
+    except Exception:
         db.rollback()
         raise HTTPException(status_code=400, detail="An error occurred while deleting the genre")
+
+
+
